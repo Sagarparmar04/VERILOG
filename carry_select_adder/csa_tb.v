110044c1aea4c20b269308013e0c6f8cce936ae7
Sagar Parmar <108988109+Sagarparmar04@users.noreply.github.com>
2023-10-05 00:01:18 +0530

    Update csa_tb.v

--- carry_select_adder/csa_tb.v
+++ carry_select_adder/csa_tb.v
@@ -2,17 +2,17 @@
 module csa_tb();
 	//-----------port direction---------------
 	//------input port-------
-  reg [3:0] A;
-  reg [3:0] B;
-  reg Cin;
+  	reg [3:0] A;
+  	reg [3:0] B;
+  	reg Cin;
 	//------output port-------
-  wire [3:0] SUM;
-  wire CARRY;  
+  	wire [3:0] SUM;
+  	wire CARRY;  
 	//------port-------
-  integer i,j;
+  	integer i,j;
 	
 	//dut	
-  csa dut(.A(A),.B(B),.Cin(Cin),.SUM(SUM),.CARRY(CARRY));
+  	csa dut(.A(A),.B(B),.Cin(Cin),.SUM(SUM),.CARRY(CARRY));
 	
 	//-----------task for monitor----------------
 	task monitor();
@@ -20,30 +20,30 @@ module csa_tb();
 	endtask
 	
 	//-----------------------------------------
-    initial begin
-			monitor();
-			A = 0;	B = 0;	Cin = 0;
+    	initial begin
+		monitor();
+		A = 0;	B = 0;	Cin = 0;
 
-      for(i=0;i<10;i=i+1) begin
-            for(j=0;j<10;j=j+1) begin
-                 A = i;
-                 B = j;
-                 #10;
-            end  
-						$stop; //terminate the loop
-      end
+      		for(i=0;i<10;i=i+1) begin
+            		for(j=0;j<10;j=j+1) begin
+                 		A = i;
+                 		B = j;
+                 		#10;
+            		end  
+			$stop; //terminate the loop
+      		end
 			
-      Cin = 1; 
+      		Cin = 1; 
 
-      for(i=0;i<10;i=i+1) begin
-            for(j=0;j<10;j=j+1) begin
-                 A = i;
-                 B = j;
-                 #10;
-            end 
-						$stop;	//terminate the loop
-      end 
-			$finish; //terminate the program
-   end
+      		for(i=0;i<10;i=i+1) begin
+            		for(j=0;j<10;j=j+1) begin
+                 		A = i;
+                 		B = j;
+                 		#10;
+           	 	end 
+			$stop;	//terminate the loop
+      		end 
+		$finish; //terminate the program
+   	end
     
 endmodule : csa_tb
