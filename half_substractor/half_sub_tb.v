72396d4e81a33c0d3871a3fb07fd0189e7e3516e
Sagar Parmar <108988109+Sagarparmar04@users.noreply.github.com>
2023-10-04 23:46:44 +0530

    Add files via upload

---
+++ half_substractor/half_sub_tb.v
@@ -0,0 +1,32 @@
+module half_sub_tb;
+
+//port direction
+reg a,b;//input port
+wire diff,borrow;//output port
+
+//dut
+half_sub dut(.a(a),
+						 .b(b),
+						 .diff(diff),
+						 .borrow(borrow));
+						 
+		//initialization
+			initial begin
+				a=0; b=0;
+				#10;
+				a=0; b=1;
+				#10;
+				a=1; b=0;
+				#10;
+				a=1; b=1;
+				#10;
+				a=1'bx; b=1'bx;
+				#10;
+				a=1'bx; b=0;
+				#10;
+				a=0; b=1'bx;
+				#10;
+				$finish;//terminate the program
+			end
+
+endmodule
